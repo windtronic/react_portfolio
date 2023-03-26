84ca2fd1561018b99eeebd8fb2e32910b06fda3a
Toni <windtronic89@gmail.com>
2023-03-26 17:48:14 -0600

    change

--- react_portfolio/src/components/Contact.jsx
+++ react_portfolio/src/components/Contact.jsx
@@ -1,8 +1,5 @@
 export default function Contact (props) {
 
-    // const [name, setName] = useState("")
-    // const [email, setEmail] = useState("")
-    // const [message, setMessage] = useState("") 
 
     return (
         <h2>Contact</h2>
