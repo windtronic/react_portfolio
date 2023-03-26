4435db43bde19799186af3c7f388ec6ec0b9f808
Toni <windtronic89@gmail.com>
2023-03-26 17:09:06 -0600

    about me page

--- react_portfolio/src/components/About.jsx
+++ react_portfolio/src/components/About.jsx
@@ -6,27 +6,15 @@ export default function About (props) {
             <div className="page-heading">
                 <h1>About Me</h1>
             </div>
-            <p>I am a Full Stack Web Developer specialising in <span className="specialisation">React.JS</span>
-             and <span className="specialisation">Ruby on Rails</span>. Additionally, I have experience with 
-             <span className="specialisation">vanilla Javascript</span>, <span className="specialisation">jQuery</span>,
-              <span className="specialisation">HTML</span> and <span className="specialisation">CSS</span> and
-               <span className="specialisation">React Native</span>.
+            <p> Hello! My name is Toni and I enjoy creating things that will be seen on the internet. 
+            I'm passionate about all things related to technology, and I'm constantly exploring new trends and techniques in software development.
+            As a software engineering student, I've gained experience in a variety of programming languages
+            including [HTML5, CSS, JavaScript, React.js, Node.js, JQuery, Sequel, Python 3.11].
             </p>
-            <p>Having worked internationally across South Asia, East Africa and the Former Soviet Union, and in the humanitarian
-                and international development sectors for several years, I am experienced in working remotely and with teams with diverse backgrounds and contributions.</p>
-            <p>Working in emergencies and in contexts with limited or developing infrastructure I have worked under pressure and resourcefully when necessary.</p>
-            <p>Projects I designed, managed and monitored have ranged in length and size, from several months to several years, and from several thousand pounds to several million.</p>
-            <p>In additional to my technical skills, I am experienced in financial management, project management, proposal and report writing, and people management.</p>
-          
-            {/* <div className="image-container-flags">
-                <img src="http://img.freeflagicons.com/thumb/round_icon/nepal/nepal_640.png" />
-                <img src="http://img.freeflagicons.com/thumb/round_icon/bangladesh/bangladesh_640.png" />
-                <img src="http://img.freeflagicons.com/thumb/round_icon/united_kingdom/united_kingdom_640.png" />
-                <img src="http://img.freeflagicons.com/thumb/round_icon/germany/germany_640.png" />
-                <img src="http://img.freeflagicons.com/thumb/round_icon/colombia/colombia_640.png" />
-            </div> */}
+            <p> I'm a strong believer in the power of collaboration, and I enjoy working with others to solve complex problems.
+             I'm also committed to continuous learning, and I'm always looking for opportunities to expand my knowledge and skills.</p>
+     
         </div><h2> About </h2></>
     )
 } 
 
-//THIS IS JUST AN EXAMPLE POPULATED TO ASSIST ME WITH HOW I WANT IT TO LOOK //
