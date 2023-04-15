92960835ce9da9d5a4d097cf1b59300a18e651a9
Toni <windtronic89@gmail.com>
2023-04-15 14:18:22 -0600

    resume is centered now

--- react_portfolio/src/components/Resume.jsx
+++ react_portfolio/src/components/Resume.jsx
@@ -1,9 +1,12 @@
 export default function Resume () {
 
     return (
-  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-  <iframe src="https://docs.google.com/document/d/e/2PACX-1vQ7PkbAxutpMkXas2USmV7QB_DoA2HVzDI8PprjcOjpPKVIhbxhwiFBQdgOE79NVQE4dJnsKtAXRrUX/pub?embedded=true" width="100%" height="600" frameborder="0"></iframe>
+  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+  <div style={{ minWidth: '60%' }}>
+    <iframe src="https://docs.google.com/document/d/e/2PACX-1vQ7PkbAxutpMkXas2USmV7QB_DoA2HVzDI8PprjcOjpPKVIhbxhwiFBQdgOE79NVQE4dJnsKtAXRrUX/pub?embedded=true" width="100%" height="600" frameborder="0"></iframe>
+  </div>
 </div>
+
 
 
 
