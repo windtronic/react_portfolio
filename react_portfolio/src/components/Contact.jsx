fe0839ca6a3c747bd9d269d85f3d18388b566ec2
Toni <windtronic89@gmail.com>
2023-04-15 14:54:01 -0600

    Contact Page is good

--- react_portfolio/src/components/Contact.jsx
+++ react_portfolio/src/components/Contact.jsx
@@ -1,8 +1,18 @@
-export default function Contact (props) {
 
 
-    return (
-        <h2>Contact</h2>
-    )
+export default function Contact() {
+  return (
+    <div className="contact-container">
+      <div className="contact-card">
+        <button onClick={() => window.open('mailto:windtronic89@gmail.com', '_blank')}>EMAIL ME</button>
+      </div>
+      <div className="contact-card">
+        <button onClick={() => window.open('https://www.linkedin.com/in/toni-hall/', '_blank')}>FIND ME ON LINKEDIN</button>
+      </div>
+      <div className="contact-card">
+        <button onClick={() => window.open('https://github.com/windtronic/', '_blank')}>FIND ME ON GITHUB</button>
+      </div>
+    </div>
+  );
+}
 
-} 
