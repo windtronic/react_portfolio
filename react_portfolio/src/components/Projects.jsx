642267edfbaf05b3ae73b8a324be45b80522e589
Toni <windtronic89@gmail.com>
2023-04-15 15:56:13 -0600

    css for projects

--- react_portfolio/src/components/Projects.jsx
+++ react_portfolio/src/components/Projects.jsx
@@ -1,8 +1,35 @@
-export default function Projects () {
+import gardeningAppImg from '../images/gardening-app.png';
+import tickItImg from '../images/tick-it.png';
+import pixiDreamsImg from '../images/pixi-dreams.png';
+import goFindAnimeImg from '../images/go-find-anime.png';
 
-    return (
-    <div>
-        <h2>Projects</h2>
+export default function Projects() {
+  return (
+    <div className="project-container">
+      <div className="project-card">
+        <a href="https://github.com/windtronic/garden_front_end" target="_blank" rel="noopener noreferrer">
+          <img src={gardeningAppImg} alt="Gardening App" width="360" height="260" />
+          <h3>My Gardening App</h3>
+        </a>
+      </div>
+      <div className="project-card">
+        <a href="https://github.com/windtronic/tickit_app_frontend" target="_blank" rel="noopener noreferrer">
+          <img src={tickItImg} alt="Tick-It" width="360" height="260" />
+          <h3>Tick-It</h3>
+        </a>
+      </div>
+      <div className="project-card">
+        <a href="https://github.com/windtronic/pixidreams-front-end" target="_blank" rel="noopener noreferrer">
+          <img src={pixiDreamsImg} alt="PixiDreams" width="360" height="260" />
+          <h3>PixiDreams</h3>
+        </a>
+      </div>
+      <div className="project-card">
+        <a href="https://github.com/windtronic/anime_search_database" target="_blank" rel="noopener noreferrer">
+          <img src={goFindAnimeImg} alt="Go Find Anime" width="360" height="260" />
+          <h3>Go Find Anime</h3>
+        </a>
+      </div>
     </div>
-    )
-} 
+  );
+}
