a0433eb788183baeaf5324b5b87137429707df76
Toni <windtronic89@gmail.com>
2023-04-15 16:04:00 -0600

    updates

--- react_portfolio/src/components/About.jsx
+++ react_portfolio/src/components/About.jsx
@@ -1,59 +1,40 @@
-
-
 export default function About() {
   return (
     <div className="about-page">
-      <div className="page-heading">
-       
-      </div>
-     
-        <h3>
-          Hello there! My name is Toni and I have a passion for creating things
-          that live on the internet. From web development to software
-          engineering, I am constantly exploring new trends and techniques to
-          enhance my skills. As a software engineering student, I have gained
-          experience in various programming languages, such as ES6 Javascript,
-           HTML5, CSS, React.js, Node.js/Express, JQuery, and Python.
-        </h3>
-   
+      <div className="page-heading"></div>
 
-     
-        <h3>
-          In addition to my software engineering background, I also have a
-          strong foundation in complex systems and creative problem-solving,
-          thanks to my background in electrical engineering focused on wind
-          turbine technology, and experience as a veteran of the United States
-          Army. I strongly believe in the power of collaboration and enjoy
-          working with others to tackle complex issues. I am committed to
-          continuous learning and constantly seeking opportunities to expand my
-          knowledge and skills.
-        </h3>
-  
+      <h3>
+        Hello there! My name is Toni and I have a passion for creating things
+        that live on the internet. From web development to software engineering,
+        I am constantly exploring new trends and techniques to enhance my
+        skills. As a full stack software engineer, I have gained experience in various
+        programming languages, such as ES6 Javascript, HTML5, CSS, React.js,
+        Node.js/Express, JQuery, and Python.
+      </h3>
 
-     
-        <h3>
-          With my unique blend of technical expertise, problem-solving
-          abilities, and leadership qualities, I believe that I can be a
-          valuable asset to any team. I am eager to continue my professional
-          growth and take on new challenges and opportunities.
-        </h3>
-    
+      <h3>
+        In addition to my software engineering background, I also have a strong
+        foundation in complex systems and creative problem-solving, thanks to my
+        background in electrical engineering focused on wind turbine technology,
+        and experience as a veteran of the United States Army. I strongly
+        believe in the power of collaboration and enjoy working with others to
+        tackle complex issues. I am committed to continuous learning and
+        constantly seeking opportunities to expand my knowledge and skills.
+      </h3>
 
-     
-        <h3>
-          In my free time, I enjoy playing guitar and writing new songs. I also
-          love fire performing (when it's warm enough!), dancing, exploring
-          musical arts, hanging out with family and friends, traveling, and
-          trying new foods.
-        </h3>
-      </div>
-   
+      <h3>
+        With my unique blend of technical expertise, problem-solving abilities,
+        and leadership qualities, I believe that I can be a valuable asset to
+        any team. I am eager to continue my professional growth and take on new
+        challenges and opportunities.
+      </h3>
+
+      <h3>
+        In my free time, I enjoy playing guitar and writing new songs. I also
+        love fire performing (when it's warm enough!), dancing, exploring
+        musical arts, hanging out with family and friends, traveling, and trying
+        new foods.
+      </h3>
+    </div>
   );
 }
-
-
-
-            
-
-            
- 
