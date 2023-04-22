457c1e328f0749aabc3ca4637d694e768ba2dd64
Toni <windtronic89@gmail.com>
2023-04-22 11:04:02 -0600

    updates

--- react_portfolio/src/components/Projects.jsx
+++ react_portfolio/src/components/Projects.jsx
@@ -1,34 +1,132 @@
-import gardeningAppImg from '../images/gardening-app.png';
-import tickItImg from '../images/tick-it.png';
-import pixiDreamsImg from '../images/pixi-dreams.png';
-import goFindAnimeImg from '../images/go-find-anime.png';
+import gardeningAppImg from "../images/gardening-app.png";
+import tickItImg from "../images/tick-it.png";
+import pixiDreamsImg from "../images/pixi-dreams.png";
+import goFindAnimeImg from "../images/go-find-anime.png";
 
 export default function Projects() {
   return (
     <div className="project-container">
       <div className="project-card">
-        <a href="https://github.com/windtronic/garden_front_end" target="_blank" rel="noopener noreferrer">
-          <img src={gardeningAppImg} alt="Gardening App" width="360" height="260" />
+        <a
+          href="https://github.com/windtronic/garden_front_end"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={gardeningAppImg}
+            alt="Gardening App"
+            width="360"
+            height="260"
+          />
           <h3>My Gardening App</h3>
         </a>
+        <p>
+          A full-stack web application that I developed using Django in Python,
+          DBeaver, React.js, CSS, and various other libraries including
+          React-Big-Calendar, Material-UI, React Spring, and React Gesture. The
+          purpose of this application is to help gardening enthusiasts plan and
+          track their gardens. As the sole developer on this project, I was
+          responsible for designing and implementing the entire application.
+          This included creating the user interface and using Reactjs and its
+          various libraries to create a smooth and intuitive user experience.
+        </p>
+        <p>
+          Technologies used: Django, Python, DBeaver, React.js, CSS,
+          React-Big-Calendar, Material-UI, React Spring, React Gesture
+        </p>
+        <p>
+          GitHub links:{" "}
+          <a href="https://github.com/windtronic/garden_front_end">Frontend</a>{" "}
+          | <a href="https://github.com/windtronic/garden_back_end">Backend</a>
+        </p>
       </div>
       <div className="project-card">
-        <a href="https://github.com/windtronic/tickit_app_frontend" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://github.com/windtronic/tickit_app_frontend"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={tickItImg} alt="Tick-It" width="360" height="260" />
           <h3>Tick-It</h3>
         </a>
+        <p>
+          A group project Tick-iT is a concert search and booking app for the
+          Denver Metropolitan area. This is a full-stack project developed
+          utilizing Django, PostgreSQL, React.js, DBeaver, and CSS. My primary
+          role on this project was creating the backend and assisting with the
+          front end components.
+        </p>
+        <p>Technologies used: Django, PostgreSQL, React.js, DBeaver, CSS</p>
+        <p>
+          GitHub links:{" "}
+          <a href="https://github.com/windtronic/tickit_app_frontend">
+            Frontend
+          </a>{" "}
+          |{" "}
+          <a href="https://github.com/windtronic/tickit_app_backend">Backend</a>
+        </p>
       </div>
       <div className="project-card">
-        <a href="https://github.com/windtronic/pixidreams-front-end" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://github.com/windtronic/pixidreams-front-end"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={pixiDreamsImg} alt="PixiDreams" width="360" height="260" />
           <h3>PixiDreams</h3>
         </a>
+        <p>
+          A group project movie blog that provides news & reviews of the latest
+          animated movies. Our blog also provides opportunities for
+          interactivity where visitors can comment or like a blog post, and
+          possibly recommend updates for more content. This full-stack PERN
+          project developed utilizing PostgreSQL, Sequelize, Express / Node.js,
+          React.js, and CSS. My primary role on this project was creating the
+          backend and assisting with front end components.
+        </p>
+        <p>
+          Techn ologies used: PostgreSQL, Sequelize, Express / Node.js,
+          React.js, CSS
+        </p>
+        <p>
+          GitHub links:{" "}
+          <a href="https://github.com/windtronic/pixidreams-front-end">
+            Frontend
+          </a>{" "}
+          |{" "}
+          <a href="https://github.com/windtronic/pixidreams-back-end">
+            Backend
+          </a>
+        </p>
       </div>
       <div className="project-card">
-        <a href="https://github.com/windtronic/anime_search_database" target="_blank" rel="noopener noreferrer">
-          <img src={goFindAnimeImg} alt="Go Find Anime" width="360" height="260" />
+        <a
+          href="https://github.com/windtronic/anime_search_database"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={goFindAnimeImg}
+            alt="Go Find Anime"
+            width="360"
+            height="260"
+          />
           <h3>Go Find Anime</h3>
         </a>
+        <p>
+          An application made using Axios, React.js, JavaScript, HTML5, and CSS.
+          The purpose of this application is to search different anime of their
+          choice, see a brief synopsis, and a link to “myanimelist” to view the
+          full information. This project pulls information from various
+          endpoints within the Jikan API. The application features a search bar
+          that permits searches by title. I used various React Hooks, including
+          useState, and useEffect to create this functionality.
+        </p>
+        <p>Technologies used: Axios, React.js, JavaScript, HTML5, CSS</p>
+        <p>
+          GitHub link:{" "}
+          <a href="https://github.com/windtronic/anime_search_database">Link</a>
+        </p>
       </div>
     </div>
   );
