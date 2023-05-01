093cd2acb283372a3bcd6dd778d84fe381ca874f
Toni <windtronic89@gmail.com>
2023-05-01 16:55:20 -0600

    css

--- react_portfolio/src/components/Resume.jsx
+++ react_portfolio/src/components/Resume.jsx
@@ -1,13 +1,14 @@
-export default function Resume () {
-
-    return (
-  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-  <div style={{ minWidth: '61%' }}>
-    <iframe src="https://docs.google.com/document/d/e/2PACX-1vQ7PkbAxutpMkXas2USmV7QB_DoA2HVzDI8PprjcOjpPKVIhbxhwiFBQdgOE79NVQE4dJnsKtAXRrUX/pub?embedded=true" width="100%" height="600" frameborder="0"></iframe>
-  </div>
-</div>
-
-
-
-  )
+export default function Resume() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <div style={{ minWidth: "61%", margin: "0 auto" }}>
+        <iframe
+          src="https://docs.google.com/document/d/e/2PACX-1vQ7PkbAxutpMkXas2USmV7QB_DoA2HVzDI8PprjcOjpPKVIhbxhwiFBQdgOE79NVQE4dJnsKtAXRrUX/pub?embedded=true"
+          width="100%"
+          height="600"
+          frameborder="0"
+        ></iframe>
+      </div>
+    </div>
+  );
 }
