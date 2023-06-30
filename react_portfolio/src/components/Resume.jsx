953ac1ec0e5a39a67d148464c2d872c8278013b5
Toni <windtronic89@gmail.com>
2023-06-30 11:53:18 -0600

    resume

--- react_portfolio/src/components/Resume.jsx
+++ react_portfolio/src/components/Resume.jsx
@@ -1,9 +1,16 @@
 export default function Resume() {
   return (
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
       <div style={{ minWidth: "61%", margin: "0 auto" }}>
         <iframe
-          src="https://docs.google.com/document/d/e/2PACX-1vQ7PkbAxutpMkXas2USmV7QB_DoA2HVzDI8PprjcOjpPKVIhbxhwiFBQdgOE79NVQE4dJnsKtAXRrUX/pub?embedded=true"
+          src="https://docs.google.com/document/d/1sY7DsSUHBWMSdkB1fbFxfvQj4SYfRpZnRVMcykSR48E/edit?embedded=true"
           width="100%"
           height="600"
           frameborder="0"
