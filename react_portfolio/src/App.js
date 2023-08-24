a86f1dfe1d5c99c21e1c6f04674297d0477758ad
Toni <windtronic89@gmail.com>
2023-08-24 13:02:45 -0600

    layout

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -1,34 +1,22 @@
 import { Routes, Route } from "react-router-dom";
-import './App.css';
-import Header from './components/Header'
-import About from './components/About'
-import Contact from './components/Contact'
-import Projects from './components/Projects'
-import Home from './components/Home'
-
+import "./App.css";
+import Header from "./components/Header";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
+import Home from "./components/Home";
 
 function App() {
-
-
-
-
   return (
     <div className="App">
-      
       <Header />
-    
-      
+
       <Routes>
-            
-            
-            <Route path='/' element={<Home />} />
-            <Route path="/About" element={<About/>} />
-            <Route path='/Contact' element={<Contact/>} />
-            <Route path='/Projects' element={<Projects/>} />
-            
-            
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Projects" element={<Projects />} />
       </Routes>
-   
     </div>
   );
 }
