14c5d0c68ef4686ebde975af1ed68fd914d97c58
Toni <windtronic89@gmail.com>
2023-08-24 12:41:47 -0600

    took resume out

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -4,7 +4,6 @@ import Header from './components/Header'
 import About from './components/About'
 import Contact from './components/Contact'
 import Projects from './components/Projects'
-import Resume from './components/Resume'
 import Home from './components/Home'
 
 
@@ -26,7 +25,7 @@ function App() {
             <Route path="/About" element={<About/>} />
             <Route path='/Contact' element={<Contact/>} />
             <Route path='/Projects' element={<Projects/>} />
-            <Route path='/Resume' element={<Resume/>} />
+            
             
       </Routes>
    
--- react_portfolio/src/components/Header.jsx
+++ react_portfolio/src/components/Header.jsx
@@ -1,26 +1,19 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-export default function Header () {
+export default function Header() {
+  return (
+    <div className="header-container">
+      <Link to="/About" className="nav-link">
+        <h1 className="header-text">About Me</h1>
+      </Link>
 
-    return (
-        <div className='header-container'>
-          
-          <Link to="/About" className="nav-link">
-                <h1 className="header-text">About Me</h1>
-            </Link>
+      <Link to="/Contact" className="nav-link">
+        <h1 className="header-text">Contact</h1>
+      </Link>
 
-            <Link to="/Contact" className="nav-link">
-                <h1 className="header-text">Contact</h1>
-            </Link>
-
-            <Link to="/Projects" className="nav-link">
-                <h1 className="header-text">Projects</h1>
-            </Link>
-
-            <Link to="/Resume" className="nav-link">
-                <h1 className="header-text">Resume</h1>
-            </Link>
-          
-        </div>
-    )
+      <Link to="/Projects" className="nav-link">
+        <h1 className="header-text">Projects</h1>
+      </Link>
+    </div>
+  );
 }
